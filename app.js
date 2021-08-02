b7b7df51e5b7555ba0d7b86125a6f4fd4abc41cd
tonnette <tonnettestanford@yahoo.com.au>
2021-08-02 18:20:28 +1000

    rotate

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const close = document.getElementById('close');
 const container = document.querySelector('.container-fluid')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', () => document.getElementById("open").style.display = "none");
+open.addEventListener('click', () => document.getElementById("close").style.display = "block");
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', () => document.getElementById("close").style.display = "none");
+close.addEventListener('click', () => document.getElementById("open").style.display = "block");
