88ca1eafabf011bd5ea174bff0bbbf2dde4f0aeb
tonnette <tonnettestanford@yahoo.com.au>
2021-08-17 14:30:21 +1000

    added auto carousel

--- script.js
+++ script.js
@@ -4,37 +4,43 @@ const container = document.querySelector('.container-fluid')
 const myNav = document.querySelector('nav')
 const more = document.querySelector('.moreText')
 const less = document.querySelector('.lessText')
-var slideIndex = 1;
-showSlides(slideIndex);
+    // var slideIndex = 1;
+    // showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+// }
+var timer = null;
+
+var slideIndex = 0;
+showSlides();
 
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+
+open.addEventListener('click', () => container.classList.add('show-nav'));
+open.addEventListener('click', stopInterval);
 open.addEventListener('click', () => document.getElementById("open").style.display = "none");
 open.addEventListener('click', () => document.getElementById("close").style.display = "block");
 open.addEventListener('click', () => myNav.style.visibility = "visible");
@@ -43,16 +49,34 @@ open.addEventListener('click', () => myNav.style.transition = "opacity 1s, visib
 
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', showSlides);
 close.addEventListener('click', () => document.getElementById("close").style.display = "none");
 close.addEventListener('click', () => document.getElementById("open").style.display = "block");
 close.addEventListener('click', () => myNav.style.visibility = "hidden");
 close.addEventListener('click', () => myNav.style.opacity = "0");
 close.addEventListener('click', () => myNav.style.transition = "opacity 1s, visibility 1s");
 
-more.addEventListener('click', () => document.querySelector(".photo2").style.display = "block");
-more.addEventListener('click', () => more.style.display = "none");
-more.addEventListener('click', () => less.style.display = "block");
+// more.addEventListener('click', () => document.querySelector(".photo2").style.display = "block");
+// more.addEventListener('click', () => more.style.display = "none");
+// more.addEventListener('click', () => less.style.display = "block");
 
-less.addEventListener('click', () => document.querySelector(".photo2").style.display = "none");
-less.addEventListener('click', () => more.style.display = "block");
-less.addEventListener('click', () => less.style.display = "none");
+// less.addEventListener('click', () => document.querySelector(".photo2").style.display = "none");
+// less.addEventListener('click', () => more.style.display = "block");
+// less.addEventListener('click', () => less.style.display = "none");
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    timer = setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+function stopInterval() {
+    clearInterval(timer)
+}
