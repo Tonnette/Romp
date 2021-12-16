f669153d262882c0807fb989444e051acdfa914b
Tonnette Stanford <tonnettestanford@yahoo.com.au>
2021-12-16 19:59:13 +1100

    updates to homepage

--- app.js
+++ app.js
@@ -4,34 +4,11 @@ const container = document.querySelector('.container-fluid')
 const myNav = document.querySelector('nav')
 const more = document.querySelector('.moreText')
 const less = document.querySelector('.lessText')
-    // var slideIndex = 1;
-    // showSlides(slideIndex);
 
-// // Next/previous controls
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
+var timer = null;
 
-// // Thumbnail image controls
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) { slideIndex = 1 }
-//     if (n < 1) { slideIndex = slides.length }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
-// }
+var slideIndex = 0;
+showSlides();
 
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
@@ -56,3 +33,20 @@ more.addEventListener('click', () => less.style.display = "block");
 less.addEventListener('click', () => document.querySelector(".photo2").style.display = "none");
 less.addEventListener('click', () => more.style.display = "block");
 less.addEventListener('click', () => less.style.display = "none");
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    timer = setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+function stopInterval() {
+    clearInterval(timer)
+}
